e4d2287fe861541ffb56c8cc08330fb35fd79d08
Prakhar Sinha <prakharsinha063@gmail.com>
2025-10-31 22:28:24 +0530

    recursion

--- Basics/recursion/basic-recursion.cpp
+++ Basics/recursion/basic-recursion.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 using namespace std;
+void n_1_backtrack(int i,int n){
+  if(i==0) return;
+  n_1_backtrack(i-1,n);
+  cout<<n-i+1<<endl;
+}
+void o_n_backtrack(int i,int n){
+  if(i<1) return;
+  o_n_backtrack(i-1,n);
+  cout<<i<<endl;
+}
 int cnt=1;
 void p1(int i,int n){
   if(i<1) return;
@@ -40,11 +50,11 @@ int main(){
   //print from n to 1
   int n;
   cin>>n;
-  p1(n,n);
+  // p1(n,n);
    //print from 1 to n using backtracking
-
+  //  o_n_backtrack(n,n);
 
    //print from n to 1 using backtracking
-
+  n_1_backtrack(n,n);
   return 0;
 }
